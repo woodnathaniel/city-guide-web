2c21f9c8a6321280401798ce5ac845ad37607272
woodnathaniel <nathanielwood002@gmail.com>
2023-08-30 01:58:11 +0000

    update 12

--- scripts/copyToDist.js
+++ scripts/copyToDist.js
@@ -2,7 +2,7 @@ const fse = require('fs-extra');
 const path = require('path');
 
 const source = path.resolve(__dirname, '../', 'dist');
-const destination = path.resolve(__dirname, '../', 'dist');
+const destination = path.resolve(__dirname, '../', 'html');
 
 //eslint-disable-next-line
 console.log("Copying html files to dist starting...");
