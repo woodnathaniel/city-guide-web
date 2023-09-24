4beb41b82122401194428ede57e06faa82ee6c48
woodnathaniel <nathanielwood002@gmail.com>
2023-09-24 04:18:54 +0000

    tour guide

---
+++ assets/js/tool-tip.js
@@ -0,0 +1,117 @@
+tippy('#route-tooltip-icon',{
+  content: 'click for your fastes route',
+  arrow: false
+});
+
+// tippy('.layers-map',{
+//   content: 'change map layer',
+//   arrow: false
+// });
+
+tippy('#open-street-view',{
+  content: 'Dark street view',
+  arrow: false,
+  distance: 5
+});
+
+tippy('#open-satelite-view',{
+  content: 'Satelite view',
+  arrow: false,
+  distance: 0
+});
+
+tippy('#street-view',{
+  content: 'Dark street view',
+  arrow: false,
+  distance: 0
+});
+
+tippy('#satelite-view',{
+  content: 'Satelite view',
+  arrow: false,
+  distance: 0
+});
+
+tippy('.fast-traffic',{
+  content: 'Less traffic congestion',
+  arrow: false,
+  distance: 0
+})
+
+tippy('.medium-traffic',{
+  content: 'Moderate trafic congestion',
+  arrow: false,
+  distance: 0
+})
+
+tippy('.slow-traffic',{
+  content: 'Heavy traffic',
+  arrow: false,
+  distance: 0
+})
+
+tippy('.symbol',{
+  content: 'road block',
+  arrow: false,
+  distance: 0
+})
+
+tippy('#car',{
+  content: 'car mode',
+  arrow: false,
+  distance: 0
+})
+
+tippy('#van',{
+  content: 'van/bus mode',
+  arrow: false,
+  distance: 0
+})
+
+tippy('#motorcycle',{
+  content: 'motorcycle mode',
+  arrow: false,
+  distance: 0
+})
+
+tippy('#walk',{
+  content: 'Pedestrian mode',
+  arrow: false,
+  distance: 0
+})
+
+tippy('#bicycle',{
+  content: 'Biking mode',
+  arrow: false,
+  distance: 0
+})
+
+tippy('#more',{
+  content: 'click to change map layers.. add and remove traffic layers..',
+  arrow: false,
+  distance: 0
+})
+
+tippy('.distanceBox',{
+  content: 'Satelite view',
+  arrow: false,
+  distance: 0
+})
+
+tippy('.timeBox',{
+  content: 'Satelite view',
+  arrow: false,
+  distance: 0
+})
+
+tippy('.step',{
+  content: 'click for turn-by-turn instrucitons',
+  arrow: false,
+  distance: 0
+})
+
+// tippy('#satelite-view',{
+//   content: 'Satelite view',
+//   arrow: false,
+//   distance: 0
+// })
