52194990c53c98fd13254ce1422f75b262448481
woodnathaniel <nathanielwood002@gmail.com>
2023-09-26 02:58:38 +0000

    rewind

--- scripts/replacePlaceholders.js
+++ scripts/replacePlaceholders.js
@@ -3,39 +3,28 @@ const path = require('path');
 const config = require('../config');
 
 console.log('Replace placeholders starting...'); //eslint-disable-line
-try{
-    const cdnRegexes = Object.keys(config.cdn).reduce((acc, key) => {
-        acc[key] = new RegExp(`/cdn.${key}`, 'gm');
-        return acc;
-    }, {});
-}catch(error){
-    console.error("An error occurred:", error);}
 
-try{
-    const keysRegexes = Object.keys(config.keys).reduce((acc, key) => {
-        acc[key] = new RegExp(`\\\${api.key.${key}}`, 'gm');
-        return acc;
-    }, {});
-    
-}catch(error){
-    console.error("An error occurred:", error);}
+const cdnRegexes = Object.keys(config.cdn).reduce((acc, key) => {
+    acc[key] = new RegExp(`/cdn.${key}`, 'gm');
+    return acc;
+}, {});
+
+const keysRegexes = Object.keys(config.keys).reduce((acc, key) => {
+    acc[key] = new RegExp(`\\\${api.key.${key}}`, 'gm');
+    return acc;
+}, {});
 
 const hostedStylesRegex = new RegExp(`\\\${hostedStylesVersion}`, 'gm'); //eslint-disable-line
-try{
-    const styleRegexes = Object.keys(config.styles).reduce((acc, customStyle) => {
-        acc[customStyle] = new RegExp(`\\\${styles.${customStyle}}`, 'gm');
-        return acc;
-    }, {});
-}catch(error){
-    console.error("An error occurred:", error);}
 
-try{
-    const productInfoRegexes = Object.keys(config.productInfo).reduce((acc, key) => {
-        acc[key] = new RegExp(`\\\${productInfo.${key}}`, 'gm');
-        return acc;
-    }, {});
-}catch(error){
-    console.error("An error occurred:", error);}
+const styleRegexes = Object.keys(config.styles).reduce((acc, customStyle) => {
+    acc[customStyle] = new RegExp(`\\\${styles.${customStyle}}`, 'gm');
+    return acc;
+}, {});
+
+const productInfoRegexes = Object.keys(config.productInfo).reduce((acc, key) => {
+    acc[key] = new RegExp(`\\\${productInfo.${key}}`, 'gm');
+    return acc;
+}, {});
 
 const regexes = {
     ...cdnRegexes,
